0f85f91a5e9ac713eb8fd70550536b88beb68041
Hugo Sanchez <hugsanchez0917@gmail.com>
2021-04-26 14:43:05 -0400

    test

--- test/spec.js
+++ test/spec.js
@@ -1,23 +1,29 @@
 const { expect } = require('chai');
 
-const { syncAndSeed } = require('../server/db/seed');
+// const { syncAndSeed } = require('../server/db/seed');
 
-const app = require('supertest')(require('../server/server'));
+// const app = require('supertest')(require('../server/server'));
 
-describe('Routes', () => {
-    //beforeEach (() => syncAndSeed());
-    describe('GET /', () => {
-        it('show info', async() => {
-            const response = await app.get('/');
-            expect(response.status).to.equal(200);
-            expect(response.text).to.include('Grace Shopper Project');
-        });
-    });
+// describe('Routes', () => {
+//     //beforeEach (() => syncAndSeed());
+//     describe('GET /', () => {
+//         it('show info', async() => {
+//             const response = await app.get('/');
+//             expect(response.status).to.equal(200);
+//             expect(response.text).to.include('Grace Shopper Project');
+//         });
+//     });
 
-    describe('GET /api/products', () => {
-        it('return products', async() => {
-            const response = await app.get('/api/products');
-            expect(response.status).to.equal(200);
-        });
-    });
+//     describe('GET /api/products', () => {
+//         it('return products', async() => {
+//             const response = await app.get('/api/products');
+//             expect(response.status).to.equal(200);
+//         });
+//     });
+// });
+
+describe('Testing', () => {
+    it('equals 2', ()=>{
+        expect(1 + 1).to.equal(2)
+    })
 });
