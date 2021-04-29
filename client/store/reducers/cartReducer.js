7a53cbf0dd96f99f5a2f5a75b1329bbae0d07d64
Manu Swami <swami.manu@gmail.com>
2021-04-29 11:02:59 -0400

    updates to cart total and css

--- client/store/reducers/cartReducer.js
+++ client/store/reducers/cartReducer.js
@@ -7,19 +7,19 @@ import ADJUST_QUANTITY from "../actionCreators/shoppingCart";
 //     quantity: 0,
 // };
 
-const initialCart= {
-    cart: []
-}
+const initialCart = {
+  cart: [],
+};
 
 export const cartReducer = (state = initialCart, action) => {
-    if (action.type === ADD_TO_CART) {
-        console.log('cart reducer', action.payload)
-        // console.log('check add action reducer',action)
-        // return { ...state, item: action.payload };
-        const {cart}= state
-        console.log('cart log',cart)
-        return {...state, cart: [...cart, action.payload]}
-    } else {
-        return state;
-    }
+  if (action.type === ADD_TO_CART) {
+    //console.log('cart reducer', action.payload)
+    // console.log('check add action reducer',action)
+    // return { ...state, item: action.payload };
+    const { cart } = state;
+    //console.log('cart log',cart)
+    return { ...state, cart: [...cart, action.payload] };
+  } else {
+    return state;
+  }
 };
