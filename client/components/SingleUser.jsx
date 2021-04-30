696be7d42640dc81d0bbd6c36360a7b03bfe6ccc
Anthony <AnthonySgro1995@gmail.com>
2021-04-30 15:50:46 -0400

    edits to single=user=component

--- client/components/SingleUser.jsx
+++ client/components/SingleUser.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 
 // Redux Imports
-const { connect } = require("react-redux");
+import { connect } from "react-redux";
+
+// React-Router Imports
+import { Switch, NavLink } from "react-router-dom";
 
 class SingleUser extends Component {
     constructor(props) {
@@ -10,20 +13,42 @@ class SingleUser extends Component {
     }
 
     render() {
-        const {
-            firstName,
-            lastName,
-            email,
-            username,
-            userType,
-        } = this.props.user;
-        return <div className="primary-screen"></div>;
+        const { user, isSignedIn } = this.props.signInInfo;
+
+        // You can only see a profile if you are signed in
+        if (!isSignedIn) {
+            return (
+                <div className="primary-screen">
+                    Please login to view your profile
+                </div>
+            );
+        }
+
+        // If signed in, get user properties
+        const { id, firstName, lastName, email, username, userType } = user;
+
+        return (
+            <div className="primary-screen">
+                <header className="user-header">
+                    <div className="user-header-container">
+                        <NavLink to={`/user/${id}/profile`} name="profile">
+                            Account
+                        </NavLink>
+                    </div>
+                    <div className="user-header-container">
+                        <NavLink to={`/user/${id}/profile`} name="orders">
+                            Orders
+                        </NavLink>
+                    </div>
+                </header>
+            </div>
+        );
     }
 }
 
 function mapStateToProps(state) {
     return {
-        user: state.singleUser,
+        signInInfo: state.signedIn,
     };
 }
 
