2213e7928f715d453edac998a26ed1997245d9c2
Anthony <AnthonySgro1995@gmail.com>
2021-04-29 23:41:21 -0400

    created single user component basics

---
+++ client/components/SingleUser.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+// Redux Imports
+const { connect } = require("react-redux");
+
+class SingleUser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        const {firstName, lastName, email,} = this.props.user;
+        return <div className="primary-screen"></div>;
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        user: state.singleUser,
+    };
+}
+
+export default connect(mapStateToProps)(SingleUser);
