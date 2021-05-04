84e48ce62962d0e87383782c1760eec508819e43
Manu Swami <swami.manu@gmail.com>
2021-05-04 18:34:24 -0400

    I kinda got stripe to work

--- server/api/stripeCheckout.js
+++ server/api/stripeCheckout.js
@@ -27,27 +27,28 @@ router.post("/", async (req, res) => {
     });
 
     const idempotencykey = uuidv4();
+    console.log("key", idempotencykey);
     const charge = await stripe.charges.create(
       {
-        //amount: product.price * 100,
+        amount: 1000,
         currency: "usd",
         customer: customer.id,
         receipt_email: token.email,
         description: `Purchased`,
-        shipping: {
-          name: token.card.name,
-          address: {
-            line1: token.card.address_line1,
-            line2: token.card.address_line2,
-            city: token.card.address_city,
-            country: token.card.address_country,
-            postal_code: token.card.address_zip,
-          },
-        },
-      },
-      {
-        idempotencykey,
+        // shipping: {
+        //   name: token.card.name,
+        //   address: {
+        //     line1: token.card.address_line1,
+        //     line2: token.card.address_line2,
+        //     city: token.card.address_city,
+        //     country: token.card.address_country,
+        //     postal_code: token.card.address_zip,
+        //   },
+        // },
       }
+      // {
+      //   idempotencykey,
+      // }
     );
     console.log("Charge:", { charge });
     status = "success";
