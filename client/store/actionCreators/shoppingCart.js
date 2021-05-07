9a5fae5ef71cc3b7689bc26e624ee2adb9c4b4df
Anthony <AnthonySgro1995@gmail.com>
2021-05-07 11:41:20 -0400

    little tweaks

--- client/store/actionCreators/shoppingCart.js
+++ client/store/actionCreators/shoppingCart.js
@@ -12,64 +12,69 @@ export const EMPTY_CART = "EMPTY_CART";
 // }
 
 export const addItemToCart = (product, userId) => {
-  return async (dispatch) => {
-    if (product === null) {
-      const response = (await axios.get(`/api/cart/productsInCart/${userId}`))
-        .data;
-      dispatch({ type: ADD_TO_CART, payload: response });
-    } else {
-      await axios.post(`/api/cart`, { product, userId });
-      const response = (await axios.get(`/api/cart/productsInCart/${userId}`))
-        .data;
-      dispatch({ type: ADD_TO_CART, payload: response });
-    }
-  };
+    return async (dispatch) => {
+        if (product === null) {
+            const response = (
+                await axios.get(`/api/cart/productsInCart/${userId}`)
+            ).data;
+            dispatch({ type: ADD_TO_CART, payload: response });
+        } else {
+            await axios.post(`/api/cart`, { product, userId });
+            const response = (
+                await axios.get(`/api/cart/productsInCart/${userId}`)
+            ).data;
+            dispatch({ type: ADD_TO_CART, payload: response });
+        }
+    };
 };
 
 export const removeFromCart = (product, userId) => {
-  return async (dispatch) => {
-    const response = (
-      await axios.delete(`/api/cart/productsInCart/${product.productId}`, {
-        data: { userId },
-      })
-    ).data;
-    dispatch({ type: REMOVE_FROM_CART, payload: response });
-  };
+    return async (dispatch) => {
+        const response = (
+            await axios.delete(
+                `/api/cart/productsInCart/${product.productId}`,
+                {
+                    data: { userId },
+                },
+            )
+        ).data;
+        dispatch({ type: REMOVE_FROM_CART, payload: response });
+    };
 };
 
 export const increaseQuantity = (product, userId) => {
-  return async (dispatch) => {
-    const response = (
-      await axios.put(
-        `/api/cart/productsInCart/increase/${product.productId}`,
-        {
-          data: { userId },
-        }
-      )
-    ).data;
-    console.log("response in reducer", response);
-    dispatch({ type: INCREASE_QUANTITY, payload: response });
-  };
+    return async (dispatch) => {
+        const response = (
+            await axios.put(
+                `/api/cart/productsInCart/increase/${product.productId}`,
+                {
+                    data: { userId },
+                },
+            )
+        ).data;
+        console.log("response in reducer", response);
+        dispatch({ type: INCREASE_QUANTITY, payload: response });
+    };
 };
 
 export const decreaseQuantity = (product, userId) => {
-  return async (dispatch) => {
-    const response = (
-      await axios.put(
-        `/api/cart/productsInCart/decrease/${product.productId}`,
-        {
-          data: { userId },
-        }
-      )
-    ).data;
+    return async (dispatch) => {
+        const response = (
+            await axios.put(
+                `/api/cart/productsInCart/decrease/${product.productId}`,
+                {
+                    data: { userId },
+                },
+            )
+        ).data;
 
-    dispatch({ type: DECREASE_QUANTITY, payload: response });
-  };
+        dispatch({ type: DECREASE_QUANTITY, payload: response });
+    };
 };
 
 export const emptyCart = () => {
-  return async (dispatch) => {
-    await axios.delete("/api/cart/emptyCart");
-    dispatch({ type: EMPTY_CART, payload: [] });
-  };
+    return async (dispatch) => {
+        await axios.delete("/api/cart/emptyCart");
+        dispatch({ type: EMPTY_CART, payload: [] });
+    };
 };
